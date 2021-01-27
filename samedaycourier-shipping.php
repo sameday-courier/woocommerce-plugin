e32215f1921ec10fbaff20d7d3f2aba6aef32af9
Alexandru Soptareanu <alexandru.soptareanu@sameday.ro>
2021-01-27 20:10:51 +0200

    Add new feature. Set a Client Unique Reference for AWB when generate the new one.

--- samedaycourier-shipping.php
+++ samedaycourier-shipping.php
@@ -4,7 +4,7 @@
  * Plugin Name: SamedayCourier Shipping
  * Plugin URI: https://github.com/sameday-courier/woocommerce-plugin
  * Description: SamedayCourier Shipping Method for WooCommerce
- * Version: 1.0.25
+ * Version: 1.0.26
  * Author: SamedayCourier
  * Author URI: https://www.sameday.ro/contact
  * License: GPL-3.0+
