d1034904b0c7b89f9c9af4230c2e91c426ec6247
Soptareanu <soptareanu_alex@yahoo.com>
2025-09-23 15:51:20 +0300

    SIP-806. Add filters for Pickup-Point grid.

--- samedaycourier-shipping.php
+++ samedaycourier-shipping.php
@@ -4,7 +4,7 @@
  * Plugin Name: SamedayCourier Shipping
  * Plugin URI: https://github.com/sameday-courier/woocommerce-plugin
  * Description: SamedayCourier Shipping Method for WooCommerce
- * Version: 1.10.10
+ * Version: 1.10.11
  * Author: SamedayCourier
  * Author URI: https://www.sameday.ro/contact
  * License: GPL-3.0+
